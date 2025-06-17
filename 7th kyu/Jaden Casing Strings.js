8c4c246d6e40a580db1601ce8a48898a441357e5
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2025-06-17 12:06:46 -0500

    Update Jaden Casing Strings.js
    refactored code

--- 7th kyu/Jaden Casing Strings.js
+++ 7th kyu/Jaden Casing Strings.js
@@ -23,3 +23,8 @@ Object.defineProperty(
    }
   }
 );
+
+// refactored:
+// String.prototype.toJadenCase = function() {
+//   return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+// };
