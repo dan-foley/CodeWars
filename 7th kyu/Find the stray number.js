452239824fa0a8dcce8eb0eba582084d91e4d16d
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2025-07-03 11:54:03 -0500

    Update Find the stray number.js
    Add simpler solution

--- 7th kyu/Find the stray number.js
+++ 7th kyu/Find the stray number.js
@@ -8,11 +8,17 @@
 // [1, 1, 2] ==> 2
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 
+// function stray(numbers) {
+//   let sorted = numbers.sort((a, b) => a - b);
+//   if (sorted[0] !== sorted[1]){
+//     return sorted[0]
+//   } else if (sorted[sorted.length - 1] !== sorted[sorted.length - 2]){
+//     return sorted[sorted.length - 1]
+//   }
+// }
+
+// Simpler solution
 function stray(numbers) {
-  let sorted = numbers.sort((a, b) => a - b);
-  if (sorted[0] !== sorted[1]){
-    return sorted[0]
-  } else if (sorted[sorted.length - 1] !== sorted[sorted.length - 2]){
-    return sorted[sorted.length - 1]
-  }
+  let sorted = numbers.sort((a,b) => a - b);
+  return sorted[0] === sorted[1] ? sorted[sorted.length-1] : sorted[0];
 }
