1bc4a7a4721a818225572698ee51e933fc89f33f
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2025-11-07 13:06:11 -0600

    Add files via upload

---
+++ 6th kyu/Encrypt this!.js
@@ -0,0 +1,23 @@
+// Description:
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+const encryptThis = text => {
+  return text.split(' ').map(word => {
+    if (word.length === 0) return '';
+    if (word.length === 1) return word.charCodeAt(0);
+    if (word.length === 2) return word.charCodeAt(0) + word[1];
+    return word.charCodeAt(0) + word[word.length-1] + word.slice(2,-1) + word[1];
+  }).join(' ');
+};
