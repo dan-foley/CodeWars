0a501a327f20fcf748538da71f139dc0c0e89853
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2025-11-12 15:46:51 -0600

    Add files via upload

---
+++ 6th kyu/Highest Rank Number in an Array.js
@@ -0,0 +1,32 @@
+// Complete the method which returns the number which is most frequent in the given input array. If there is a tie for most frequent number, return the largest number among them.
+
+// Note: no empty arrays will be given.
+
+// Examples
+// [12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+// [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+// [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+
+function highestRank(arr){
+  let nums = {};
+  arr.forEach(n => {
+    nums[n] = (nums[n] || 0) + 1;
+  });
+  
+  let maxFreq = 0;
+  let mostFreqNum = 0;
+  
+  for (let n in nums){
+    n = Number(n);
+    
+    if (nums[n] > maxFreq){
+      maxFreq = nums[n];
+      mostFreqNum = n;
+    } else if (nums[n] === maxFreq){
+      if (n > mostFreqNum){
+        mostFreqNum = n;
+      };
+    };
+  };
+  return mostFreqNum;
+};
