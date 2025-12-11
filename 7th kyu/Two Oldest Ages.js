c4b2b1d7948ac2ac9ce58985e309e282ef5cb47e
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2025-12-11 14:57:19 -0600

    Refactor twoOldestAges function to arrow syntax

--- 7th kyu/Two Oldest Ages.js
+++ 7th kyu/Two Oldest Ages.js
@@ -8,6 +8,8 @@
 // [1, 5, 87, 45, 8, 8] --> [45, 87]
 // [1, 3, 10, 0]) --> [3, 10]
 
-function twoOldestAges(ages){
-  return ages.sort((a,b)=>a-b).slice(-2);
-}
+// function twoOldestAges(ages){
+//   return ages.sort((a,b)=>a-b).slice(-2);
+// }
+
+const twoOldestAges = ages => ages.sort((a,b)=>a-b).slice(-2);
