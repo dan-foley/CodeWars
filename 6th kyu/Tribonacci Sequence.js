28320c00360e988f4458f1aabc09e575083cfff6
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2026-02-02 13:30:51 -0600

    Refactor tribonacci function implementation
    Refactor tribonacci function to modify signature array directly and return the first n elements.

--- 6th kyu/Tribonacci Sequence.js
+++ 6th kyu/Tribonacci Sequence.js
@@ -13,10 +13,8 @@
 // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
 
 function tribonacci(signature,n){
-  let result = signature.slice(0, n);
-  for (let i = 3; i < n; i++) {
-    const nextNum = result[i - 1] + result[i - 2] + result[i - 3];
-    result.push(nextNum);
+  for (let i = 3; i < n; i++){
+    signature.push(signature[i-1] + signature[i-2] + signature[i-3]);
   }
-  return result;
+  return signature.slice(0, n);
 }
