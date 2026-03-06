bc0e5a198c402f27393ad2e647908a62d445e687
Dan  Foley <144731705+dan-foley@users.noreply.github.com>
2026-03-06 12:12:07 -0600

    Refactor streetFighterSelection function
    Refactor streetFighterSelection to use map for moves.

--- 6th kyu/Street Fighter 2 - Character Selection.js
+++ 6th kyu/Street Fighter 2 - Character Selection.js
@@ -1,17 +1,28 @@
 //https://www.codewars.com/kata/5853213063adbd1b9b0000be
 
-function streetFighterSelection(fighters, position, moves){
-  let [row, col] = position;
-  const result = [];
+// function streetFighterSelection(fighters, position, moves){
+//   let [row, col] = position;
+//   const result = [];
   
-  for (let move of moves){
+//   for (let move of moves){
+//     if (move === 'up') row = 0;
+//     if (move === 'down') row = 1;
+//     if (move === 'left') col === 0 ? col = 5 : col--;
+//     if (move === 'right') col === 5 ? col = 0 : col++;
+    
+//     result.push(fighters[row][col]);
+//   };
+  
+//   return result;
+
+// };
+
+function streetFighterSelection(fighters, [row,col], moves){
+  return moves.map(move => {
     if (move === 'up') row = 0;
     if (move === 'down') row = 1;
     if (move === 'left') col === 0 ? col = 5 : col--;
-    if (move === 'right') col === 5 ? col = 0 : col++;
-    
-    result.push(fighters[row][col]);
-  };
-  
-  return result;
-};
+    if (move === 'right') col === 5 ? col = 0: col++; 
+    return fighters[row][col];
+  });
+}
